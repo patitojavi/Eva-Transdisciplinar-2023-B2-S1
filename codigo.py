345e95bcd16ac6e82a5f855c807285ba7a09a557
RohanCas <rolando.castro2023@alu.uct.cl>
2023-05-30 11:40:12 -0400

    Funciones Aritmeticas.
    principalmente creamos funciones con librería math, ocupamos las formulas requeridas para la operación que queremos solución.

--- codigo.py
+++ codigo.py
@@ -1,1 +1,15 @@
-print("hola wuajaja")
+import math
+
+def calcular_friccion(masa, angulo):
+    if angulo > 35:
+        aceleracion = 9.8 * math.sin(math.radians(angulo))
+        friccion = masa * aceleracion
+        return friccion
+    else:
+        return 0
+
+def calcular():
+    masa = float(masa_entry.get())
+    angulo = float(angulo_entry.get())
+    friccion = calcular_friccion(masa, angulo)
+    resultado_label.config(text=f"La fricción es: {friccion} N")
